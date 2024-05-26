fd9bd79d994aede6486c054528ebed0467b77564
nehalkhaled19 <nehalkhaled62@gmail.com>
2024-05-26 23:51:35 +0300

    initial commit

---
+++ dist/compiled/js/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function calc(num1, num2) {
+    return num1 + num2;
+}
+console.log(calc(10, 20));
+console.log(calc(+true, +true));
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function calc(num1: number, num2: number): number {
+return num1 + num2
+}
+
+console.log(calc(10, 20));
+
+console.log(calc(+true, +true));
