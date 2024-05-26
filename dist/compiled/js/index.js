1d631c674abefef64b3eb4242513b800469afb86
nehalkhaled19 <nehalkhaled62@gmail.com>
2024-05-26 23:56:13 +0300

    arr commit

--- dist/compiled/js/index.js
+++ dist/compiled/js/index.js
@@ -4,3 +4,11 @@ function calc(num1, num2) {
 }
 console.log(calc(10, 20));
 console.log(calc(+true, +true));
+function printInfo(valueOne, valueTwo) {
+    return `Value One Is ${valueOne}, Value Two Is ${valueTwo}`;
+}
+console.log(printInfo(10, 20)); // Value One Is 10, Value Two Is 20
+console.log(printInfo("10", "20")); // Value One Is "10", Value Two Is "20"
+// console.log(printInfo(true, [1, 2, 3]));
+let arr;
+arr = [20, [true], ["n", ["n", 3]]];
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 function calc(num1: number, num2: number): number {
-return num1 + num2
+    return num1 + num2
 }
 
 console.log(calc(10, 20));
 
 console.log(calc(+true, +true));
+
+
+function printInfo(valueOne: number | string, valueTwo: number | string) {
+    return `Value One Is ${valueOne}, Value Two Is ${valueTwo}`;
+}
+console.log(printInfo(10, 20)); // Value One Is 10, Value Two Is 20
+console.log(printInfo("10", "20")); // Value One Is "10", Value Two Is "20"
+// console.log(printInfo(true, [1, 2, 3]));
+
+let arr: (number | boolean[] | (string | (string | number)[])[])[];
+arr = [20, [true], ["n", ["n", 3]]]
